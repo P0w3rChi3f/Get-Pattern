e7049e69d74428d64adf7a848d45d329987d0c1f
P0w3rChi3f <honeycutt.james@gmail.com>
2021-04-10 06:42:57 -0400

    still working on it

--- Get-Pattern.ps1
+++ Get-Pattern.ps1
@@ -4,13 +4,24 @@ A set consists of 3 bytes,  capital Letter, lowercase letter, number.  Another w
 
 Based on the output from the Ruby script, I will need a triple loop. Most inner loop (0-9) will loop first, then the middle (a-z) will loop after a full loop of the most inner loop, the the most outter loop will rotate after 26 rotations of the most inner loop or 1 rotation of the middle loop
 #>
+$maxLoops = 3
+$iterator = 0
 
 
 foreach ($cap in ('A'..'Z')) {
+    $iterator += 1
     foreach ($lower in ('a'..'z')) {
         foreach ($num in (0..9)) {
             $pattern = $pattern + $cap+$lower+$num
+            if ($iterator -ge $maxLoops) {
+                break
+            }
+            else {
+                write-host $iterator
+            }
         }# Close inner Foreach
     } # Close middle Foreach
 } # Close Outer Foreach
+
+
 $pattern
